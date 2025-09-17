0829d4e35d5d8a555ed3caf8be40359c0f81afa7
Jayabalaji <jayabalaji711@gmail.com>
2025-09-17 20:07:31 +0530

    Update wsgi.py

--- Hospital/wsgi.py
+++ Hospital/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hospital.settings')
 
+
+
+path = '/home/jayabalaji1011/hospital'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hospital.settings'
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
