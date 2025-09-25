1e51dd46a96da05579269fe6d73dff47d52162b1
Jayabalaji <jayabalaji711@gmail.com>
2025-09-25 20:09:27 +0530

    Update wsgi.py

--- Hospital/wsgi.py
+++ Hospital/wsgi.py
@@ -6,15 +6,16 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
-import os
 import sys
-from django.core.wsgi import get_wsgi_application
+import os
 
-# Add your project path
-path = '/home/jayabalaji2k/DigitalBank'
+# Add your project folder to sys.path
+path = '/home/jayabalaji2k/Digital-Bank'
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Hospital.settings'
+# Set correct settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Bank.settings'  # Use your project folder name
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
